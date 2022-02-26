6c2ac62ead3a49be093126fb074a04045503c79e
Anupam Arya <anu9am@gmail.com>
2022-02-27 00:37:18 +0530

    basic search bar and submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const APP_ID = "ebb46844"
+  const APP_KEY = "f64fd52c3ff4771ade711e7e2138f695"
+
+  useEffect(() => { console.log("Effect has been run") })
+
+  return <div className="App">
+    <form className="search-form">
+      <input className="search-bar" type="text" placeholder="Search query here!" />
+      <button className="search-button" type="submit">
+        Search
+      </button>
+    </form>
+  </div>;
 }
 
 export default App;
