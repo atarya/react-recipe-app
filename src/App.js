15d24898814e8a92cf0d1ff6909115d5d084e2c5
Anupam Arya <anu9am@gmail.com>
2022-02-27 00:45:40 +0530

    added useEffect and useState

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ function App() {
   const APP_ID = "ebb46844"
   const APP_KEY = "f64fd52c3ff4771ade711e7e2138f695"
 
-  useEffect(() => { console.log("Effect has been run") })
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect has been run");
+  }, [])
 
   return <div className="App">
     <form className="search-form">
       <input className="search-bar" type="text" placeholder="Search query here!" />
-      <button className="search-button" type="submit">
+      <button
+        className="search-button" type="submit">
         Search
       </button>
     </form>
+    <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
   </div>;
 }
 
